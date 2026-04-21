304c03ceb90d4ba79ab1b58532bb832ca8dd771c
Shreecharana24 <shreecharana.neeramoole@gmail.com>
2026-04-21 22:19:37 +0530

    Phase4->2

--- commit.c
+++ commit.c
@@ -43,14 +43,14 @@ int commit_parse(const void *data, size_t len, Commit *commit_out) {
     int rc = -1;
 
     // "tree <hex>\n"
-    if (sscanf(p, "tree %64s\n", hex) != 1) return -1;
-    if (hex_to_hash(hex, &commit_out->tree) != 0) return -1;
+    if (sscanf(p, "tree %64s\n", hex) != 1) goto cleanup;
+    if (hex_to_hash(hex, &commit_out->tree) != 0) goto cleanup;
     p = strchr(p, '\n') + 1;
 
     // optional "parent <hex>\n"
     if (strncmp(p, "parent ", 7) == 0) {
-        if (sscanf(p, "parent %64s\n", hex) != 1) return -1;
-        if (hex_to_hash(hex, &commit_out->parent) != 0) return -1;
+        if (sscanf(p, "parent %64s\n", hex) != 1) goto cleanup;
+        if (hex_to_hash(hex, &commit_out->parent) != 0) goto cleanup;
         commit_out->has_parent = 1;
         p = strchr(p, '\n') + 1;
     } else {
