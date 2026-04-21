85fc3354fb94bbbd2771966e6cab784760865039
Shreecharana24 <shreecharana.neeramoole@gmail.com>
2026-04-21 22:01:54 +0530

    Phase2->5

--- tree.c
+++ tree.c
@@ -265,8 +265,12 @@ static int write_tree_level(const Index *index, const char *prefix, ObjectID *id
 //
 // Returns 0 on success, -1 on error.
 int tree_from_index(ObjectID *id_out) {
-    // TODO: Implement recursive tree building
-    // (See Lab Appendix for logical steps)
-    (void)id_out;
-    return -1;
+    if (!id_out) return -1;
+
+    Index index;
+    if (load_index_snapshot(&index) != 0) {
+        return -1;
+    }
+
+    return write_tree_level(&index, "", id_out);
 }
